be7f10e3be77ed330f61e4828f35e648bde78b7a
rcsoliz <robertoucbnh@gmail.com>
2021-05-12 11:52:08 -0400

    initial commint template

---
+++ ItemTemplateSolution/ClassTemplates/AssemblyData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTemplates
+{
+    public class AssemblyData
+    {
+        public string proccessdDataResult { get; set; }
+        public string proccessdDataPrimaryKeyResult { get; set; }
+
+        public void GetData(object maxId, int range, ref DataSet returnedDataSet)
+        {
+            SqlDataAdapter objDataAdapter = new SqlDataAdapter();
+            objDataAdapter.Fill(returnedDataSet, "tableName");
+        }
+
+        public void ProcessdData(object primaryKey)
+        {
+            if (primaryKey != null)
+            {
+                proccessdDataResult = "1000000"; //2021-01-01
+            }
+        }
+
+        public void ExecuteData(object primaryKey, string proccessdData)
+        {
+            proccessdDataPrimaryKeyResult = "1000000"; //2021-01-01
+        }
+
+    }
+}
---
+++ ItemTemplateSolution/ClassTemplates/Simple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTemplates
+{
+    public class Simple
+    {
+        public Simple()
+        {
+
+        }
+        public void add() { }
+        public void update() { }
+        public void delete() { }
+        public void  get(Int32 id) { }
+        public void getAll() { }
+
+    }
+}
